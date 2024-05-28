b3b28593dce3b31d5a9052c8e85289b4578ade52
sayeemuddin <apdd2003@yahoo.co.in>
2024-05-28 23:37:10 +0530

    update req.txt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
-from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+# from chatterbot import ChatBot
+# from chatterbot.trainers import ListTrainer
 
 # Create a new instance of a ChatBot
 # bot = ChatBot('SurveyBot')
